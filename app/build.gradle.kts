1ba1b93fdda80ce3db860f1173678237b76d157b
guilty <sunymon@protonmail.com>
2024-08-03 22:18:36 -0400

    chore(deps): update latest & bump to sdk 35(Android 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
 	namespace = "gg.firmament.seer"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "gg.firmament.seer"
 		minSdk = 24
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0.0"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
